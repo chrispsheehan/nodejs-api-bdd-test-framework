3a3d6125ebf65a2a3298015196758d49d305399c
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-01-05 17:29:54 +0000

    fix

---
+++ src/features/step_definitions/contains_profanity/assert_contains_profanity_steps.js
@@ -0,0 +1,10 @@
+const { Then } = require("@cucumber/cucumber");
+const assert = require('chai').assert
+
+Then("profanitys are found", function() {
+  assert.isTrue(true);
+});
+
+Then("no profanitys are found", function() {
+    assert.isTrue(true);
+});
