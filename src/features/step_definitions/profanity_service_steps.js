ad6f058725987b01741df85e583da4791bbf68ec
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-01-06 19:46:49 +0000

    (very) basic first api test

--- src/features/step_definitions/profanity_service_steps.js
+++ src/features/step_definitions/profanity_service_steps.js
@@ -1,6 +1,17 @@
 const { Given } = require("@cucumber/cucumber");
-const assert = require('chai').assert
 
-Given('The profanity removing API is available', function() {
-  assert.isTrue(true);
+const superTest = require('supertest');
+const expect = require('chai').expect;
+
+const url = 'https://www.purgomalum.com';
+const purgomalum = superTest(url);
+
+
+Given('The profanity removing API is available', async function () {
+    let response = await purgomalum
+    .get('/')
+    .then(response => {
+        return response;
+    });
+    expect(response.status, 'Status Successful').to.equal(200);
 });
