f8c7a1626bcc32ee2f4cc2595f13d1164007998d
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-01-21 16:19:11 +0000

    remove unused code

--- src/features/step_definitions/profanity_service_steps.js
+++ src/features/step_definitions/profanity_service_steps.js
@@ -3,9 +3,6 @@ const expect = require('chai').expect;
 
 Given('The profanity removing API is available', async function () {
     let response = await this.api
-    .get('/')
-    .then(response => {
-        return response;
-    });
+    .get()
     expect(response.status, 'Status Successful').to.equal(200);
 });
