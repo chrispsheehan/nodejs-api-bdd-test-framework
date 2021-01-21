d626aba0c9c2939790631b729883dadd7a305892
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-01-21 16:05:34 +0000

    assert not needed

--- src/features/step_definitions/recieved_message_steps.js
+++ src/features/step_definitions/recieved_message_steps.js
@@ -1,5 +1,4 @@
 const { When } = require("@cucumber/cucumber");
-const assert = require('chai').assert
 
 When('I receive an incomming text of {string}', function(message) {
   this.message = message;
