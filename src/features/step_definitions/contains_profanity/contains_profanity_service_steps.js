205618468b6ed5a53f122e8e2216f2d6eca370ed
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-01-22 08:42:36 +0000

    check content basic

--- src/features/step_definitions/contains_profanity/contains_profanity_service_steps.js
+++ src/features/step_definitions/contains_profanity/contains_profanity_service_steps.js
@@ -1,10 +1,13 @@
 const { Given } = require("@cucumber/cucumber");
-const assert = require('chai').assert
 
-Given('I am using the contains profanity service', function() {
+Given('I am using the contains profanity service', async function() {
     this.setService("containsprofanity");
 });
 
-Given('I check the content for profanitys', function() {
-    assert.isTrue(true);
+Given('I check the content for profanitys', async function() {
+    this.request
+        .get("text=" + this.messageText)
+        .expect(200)
+        .catch(err => 
+            console.log("Could not contact service " + err));
 });
