52af75760ba49f096eca89f2b3a789179ebc110f
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-02-10 17:18:06 +0000

    status class

---
+++ src/features/purgomalum.service/purgomalumStatus.js
@@ -0,0 +1,8 @@
+const PurgomalumBase = require('./purgomalumBase.js');
+
+module.exports = class PurgomalumStatus extends PurgomalumBase {
+    
+    constructor(options) {
+        super(options);
+      }
+}
