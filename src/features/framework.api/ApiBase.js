9fcfa93096f6cab39507d23cf4ba65432014f163
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-02-11 17:05:35 +0000

    string literals

--- src/features/framework.api/ApiBase.js
+++ src/features/framework.api/ApiBase.js
@@ -5,7 +5,7 @@ module.exports = class ApiBase {
     constructor(options) {
 
         this.baseUrl = options.baseurl;
-        this.api = this.baseUrl + '/' + options.endpoint + '/';
+        this.api = `${this.baseUrl}/${options.endpoint}/`;
     }
 
     setService(serviceName, requestType) {
@@ -15,22 +15,21 @@ module.exports = class ApiBase {
 
     async isAvailable() {
         
-        console.log('\r\nChecking ' + this.baseUrl + ' is available');
+        console.log(`\r\nChecking ${this.baseUrl} is available`);
 
         return await request(this.baseUrl)
             .get("/")
             .expect(200)
             .catch(err => {
-                console.log("Could not contact service " + err);
+                console.log(`Could not contact service ${err}`);
                 return false;})
             .then(() => {
                 return true;});
       }
       
       async getResponse(paramsArray) {
-        
-        console.log('\r\nGetting response from api' + this.api + this.serviceName);
-        console.log('Params ' + JSON.stringify(paramsArray));
+        console.log(`\r\nGetting response from api ${this.api}${this.serviceName}`);
+        console.log(`Params ${JSON.stringify(paramsArray)}`);
 
         return await request(this.api)
           .get(this.serviceName)
