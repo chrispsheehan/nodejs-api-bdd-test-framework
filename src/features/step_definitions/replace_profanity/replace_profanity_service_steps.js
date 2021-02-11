5fe21d3f33a179ee2767c4e288e58c6e20eb0b54
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-02-11 15:28:31 +0000

    reorder bdd more readable

--- src/features/step_definitions/replace_profanity/replace_profanity_service_steps.js
+++ src/features/step_definitions/replace_profanity/replace_profanity_service_steps.js
@@ -4,14 +4,14 @@ Given('I am using the profanity replacement service', function() {
     this.purgomalumReplaceService.setDefaultService();
 });
 
-When('I replace profanitys in the content', async function() {   
-    this.result = await this.purgomalumReplaceService.process(this.messageText);
-});
-
 Given('I am using the profanity character replacement service with {string}', function(replacementCharacter) {
     this.purgomalumReplaceService.setReplaceCharacterService(replacementCharacter);
 });
 
 Given('I am using the profanity string replacement service with {string}', function (replacementString) {
     this.purgomalumReplaceService.setReplaceStringService(replacementString);
+});
+
+When('I replace profanitys in the content', async function() {   
+    this.result = await this.purgomalumReplaceService.process(this.messageText);
 });
