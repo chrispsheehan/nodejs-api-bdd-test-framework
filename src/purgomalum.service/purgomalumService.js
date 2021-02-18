945c61bb809fedfde0d79799f5f9f825b8437b3e
Chris Sheehan <chris_sheehan@hotmail.co.uk>
2021-02-18 15:09:37 +0000

    use promises for available check

--- src/purgomalum.service/purgomalumService.js
+++ src/purgomalum.service/purgomalumService.js
@@ -2,18 +2,33 @@ const ApiBase = require('../framework.api/ApiBase');
 
 module.exports = class PurgomalumService extends ApiBase {
 
-    constructor(options) {
-        super(options);
+  constructor(options) {
         
-        this.textProcessParamName = options.textprocessparamname;
-      }
+    super(options);
+        
+    this.textProcessParamName = options.textprocessparamname;
+  }
 
 
-    getProcessedTextResponse(messageText, additionalParams) {      
-      var params = {
-        [this.textProcessParamName] : messageText
-      };
+  getProcessedTextResponse(messageText, additionalParams) {      
       
-      return this.getResponse({...params, ...additionalParams });
-    }
+    var params = {
+      [this.textProcessParamName] : messageText
+    };
+      
+    return this.getResponse({...params, ...additionalParams });
+  }
+
+  isAvailable() {
+  
+    return new Promise((resolve, reject) => {
+      this.isServiceAvailable()
+        .then(() => {
+          return resolve(true);
+        })
+        .catch(() => {
+          return reject(false);
+        })
+    });
+  }
 }
